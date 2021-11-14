562ce5a179ea211f7dd36c644e79bddb51610b16
rafy <jinhlee19@gmail.com>
2021-11-14 12:33:56 +0900

    added gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -135,6 +135,16 @@ app.get("/about", function(req, res){
 
 // app.listen(3000, function() {
   //PORT = dynamic 자동으로 알아서 할당할수 있게 해주는듯.
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port =3000;
+}
+
+app.listen(port, function(){
+  console.log("Server has started successfully.")
 });
+
+// app.listen(process.env.PORT || 3000, function() {
+//   app.listen(3000, function() {
+//   console.log("Server started on port 3000");
+// });
