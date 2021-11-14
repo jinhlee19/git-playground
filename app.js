6a907488ae74b05d815d922bc1a19657f585e3de
rafy <jinhlee19@gmail.com>
2021-11-14 12:24:29 +0900

    savepoint1.6

--- app.js
+++ app.js
@@ -135,6 +135,6 @@ app.get("/about", function(req, res){
 
 // app.listen(3000, function() {
   //PORT = dynamic 자동으로 알아서 할당할수 있게 해주는듯.
-app.listen(process.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
